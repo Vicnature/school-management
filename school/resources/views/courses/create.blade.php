f2b9cab3b01ad160f28c94bd027ffbded84e1b7a
vicnature <ombebavictor22@gmail.com>
2024-03-07 05:57:56 +0300

    coure functionalities

--- school/resources/views/courses/create.blade.php
+++ school/resources/views/courses/create.blade.php
@@ -2,18 +2,18 @@
 @section('content')
  
 <div class="card">
-  <div class="card-header">Couse Page</div>
+  <div class="card-header">Courses Page</div>
   <div class="card-body">
       
-      <form action="{{ url('students') }}" method="post">
+      <form action="{{ url('courses') }}" method="post">
         {{-- {!! csrf_field() !!} --}}
         @csrf
         <label>Name</label></br>
         <input type="text" name="name" id="name" class="form-control"></br>
-        <label>Address</label></br>
-        <input type="text" name="address" id="address" class="form-control"></br>
-        <label>Mobile</label></br>
-        <input type="text" name="mobile" id="mobile" class="form-control"></br>
+        <label>Syllabus</label></br>
+        <input type="text" name="syllabus" id="syllabus" class="form-control"></br>
+        <label>Duration</label></br>
+        <input type="text" name="duration" id="duration" class="form-control"></br>
         <input type="submit" value="Save" class="btn btn-success"></br>
     </form>
    
--- school/routes/web.php
+++ school/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,3 +23,6 @@ Route::get('/', function () {
 Route::resource('/students',StudentController::class);
 
 Route::resource('/teachers',TeacherController::class);
+
+Route::resource('/courses',CourseController::class);
+
